769b9210291598fdf8f28f4fab1832c4e80ea470
Kirill Filonchuk <kir.realtor@gmail.com>
2023-01-16 23:17:07 +0200

    ready

---
+++ contacts.js
@@ -0,0 +1,52 @@
+const fs = require("fs/promises");
+
+const path = require("path");
+
+const contactsPath = path.join(__dirname, ".", "db", "contacts.json");
+
+async function listContacts() {
+  const dataString = await fs.readFile(contactsPath);
+  const data = JSON.parse(dataString);
+  console.log(data, "read  contacts.json");
+  return data;
+}
+
+async function getContactById(contactId) {
+  const idNormolize = contactId.toString();
+  const dataString = await fs.readFile(contactsPath);
+  const data = JSON.parse(dataString);
+  const findById = data.filter((item) => item.id === idNormolize);
+  console.log(findById);
+  return findById;
+}
+
+async function removeContact(contactId) {
+  const idNormolize = contactId.toString();
+  const dataString = await fs.readFile(contactsPath);
+  const data = JSON.parse(dataString);
+  const idxRemuveCont = data.findIndex((item) => item.id === idNormolize);
+  if (idxRemuveCont === -1) {
+    console.log(`Can not find contact by id #${contactId}`);
+    return null;
+  }
+  const newContactsList = data.filter((item) => item.id !== idNormolize);
+  await fs.writeFile(contactsPath, JSON.stringify(newContactsList, null, 2));
+  return data[idxRemuveCont];
+}
+
+async function addContact(name, email, phone) {
+  const id = Math.floor(Date.now() * Math.random()).toString();
+  const addContact = { name, email, phone };
+  const dataString = await fs.readFile(contactsPath);
+  const data = JSON.parse(dataString);
+  data.push({ id, ...addContact });
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return data;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
